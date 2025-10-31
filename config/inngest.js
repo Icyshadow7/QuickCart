693b3bfaa96bbd0d5c32c4cdb24c4cbf87975e49
Aashish Maharjan <aashishmaharjan48@gmail.com>
2025-10-31 23:52:33 +0545

    chore: update deprecated package

--- config/inngest.js
+++ config/inngest.js
@@ -1,60 +1,60 @@
-import { Inngest, InngestFunction } from "inngest";
+import { Inngest } from "inngest";
 import connectToDB from "./db";
-import { comment } from "postcss/lib/postcss";
+import User from "./models/User"; // Make sure to import your User model
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "SleekCart-next" });
+export const inngest = new Inngest({ id: "quickcart-next" });
+
 export const syncUserCreation = inngest.createFunction(
-    {
-        id: 'sync-user-from-clerk'
-    },
-    {
-        event: 'clerk/user.created '},
-    async ({ event }) => {
-        const { id, first_name,
-            last_name, image_url }=event.data
-            const userData={
-                _id:id,
-                emali:email_adderesses,
-                name:first_name+''+last_name,
-                imageURL:image_url
-            }
-            await connectToDB()
-            await User.create(userData)
-    })
+  {
+    id: 'sync-user-from-clerk'
+  },
+  {
+    event: 'clerk/user.created' // Removed extra space
+  },
+  async ({ event }) => {
+    const { id, first_name, last_name, image_url, email_addresses } = event.data; // Fixed variable name
+    const userData = {
+      _id: id,
+      email: email_addresses[0]?.email_address, // Fixed typo and added proper email access
+      name: `${first_name} ${last_name}`, // Better string concatenation
+      imageURL: image_url
+    };
+    await connectToDB();
+    await User.create(userData);
+  }
+);
 
-export const syncUserCreation =inngest.createFunction(
-    {
-id:'update-user-from-clerk'
-},
-{
-    event:'clerk/user.updated'
-},
-async({event}) =>{
-      const { id, first_name,
-            last_name, image_url }=event.data
-            const userData={
-                _id:id,
-                emali:email_adderesses,
-                name:first_name+''+last_name,
-                imageURL:image_url
-            }
+export const syncUserUpdate = inngest.createFunction( // Changed function name
+  {
+    id: 'update-user-from-clerk'
+  },
+  {
+    event: 'clerk/user.updated'
+  },
+  async ({ event }) => {
+    const { id, first_name, last_name, image_url, email_addresses } = event.data;
+    const userData = {
+      email: email_addresses[0]?.email_address, // Fixed typo
+      name: `${first_name} ${last_name}`,
+      imageURL: image_url
+    };
     
-await connectDB()
+    await connectToDB(); // Fixed function name
+    await User.findByIdAndUpdate(id, userData); // Fixed method name
+  }
+);
 
-await User.findByIDAndUpdate(id,userData)
-}
-)
-export const syncUserDeletion =inngest.createFunction(
-    {
-    id:'delete-user-from-clerk'
-    },
-    {
-        event:'clerk/user.deleted'
-    },
-    async({event})=>{
-        const{id}=event.data
-        await connectToDB()
-        await User.findByIDAndUpdate(id)
-    }
-)
+export const syncUserDeletion = inngest.createFunction(
+  {
+    id: 'delete-user-from-clerk'
+  },
+  {
+    event: 'clerk/user.deleted'
+  },
+  async ({ event }) => {
+    const { id } = event.data;
+    await connectToDB();
+    await User.findByIdAndDelete(id); // Fixed method name - use delete instead of update
+  }
+);
